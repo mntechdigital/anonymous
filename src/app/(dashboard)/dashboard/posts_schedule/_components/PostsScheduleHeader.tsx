4cd3a78a0b2d60ec4a71293dd58698cfa6cc77ad
reshad21 <miakala7@gmail.com>
2025-11-29 12:39:06 +0600

    post_schedular page create

--- src/app/(dashboard)/dashboard/posts_schedule/_components/PostsScheduleHeader.tsx
+++ src/app/(dashboard)/dashboard/posts_schedule/_components/PostsScheduleHeader.tsx
@@ -3,11 +3,11 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Plus } from 'lucide-react';
 import { useState } from 'react';
+import CreatePostModal from './CreatePostModal';
 
 const PostsScheduleHeader = () => {
     const [modal, setModal] = useState(false)
     const handleNewPost = () => {
-        console.log('New Post button clicked');
         setModal(true)
     }
     return (
@@ -29,11 +29,12 @@ const PostsScheduleHeader = () => {
                 </Select>
             </div>
             <div className='col-span-1'>
-                <button className="flex items-center gap-2 px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-full transition-colors duration-200">
+                <button className="flex items-center gap-2 px-6 py-3 bg-blue-500 hover:bg-blue-600 text-white font-medium rounded-full transition-colors duration-200 cursor-pointer" onClick={handleNewPost}>
                     <Plus size={20} />
                     Add New Post
                 </button>
             </div>
+            <CreatePostModal isOpen={modal} onClose={() => setModal(false)} />
         </div>
     );
 };
